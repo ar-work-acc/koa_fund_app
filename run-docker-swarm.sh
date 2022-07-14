08a5cd7d49e0d19324ce446582e12c9aa6694720
Louis Huang <ar.work.acc@gmail.com>
2022-07-14 18:46:48 +0800

    Swarm: update replicas in script instead (to use a common yml file)

--- run-docker-swarm.sh
+++ run-docker-swarm.sh
@@ -15,4 +15,8 @@ multipass transfer docker-compose.yml node1:
 echo 'Docker: deploy stack to Swarm'
 multipass exec node1 -- docker stack deploy -c docker-compose.yml koa_fund_app
 
+echo 'Update replicas from 1 to 3'
+multipass exec node1 -- docker service update --replicas 3 koa_fund_app_app_funds
+multipass exec node1 -- docker service update --replicas 3 koa_fund_app_nginx_node
+
 echo 'Done!'
